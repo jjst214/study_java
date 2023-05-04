1faa9ed2b46c97f45b51e8838ae98832df098dff
jjst214 <jjst214@gmail.com>
2023-05-04 09:47:26 +0900

    0503 라이브러리, 모듈, 예외처리

--- thisisjava/src/day12/exercise/WrongPasswordException.java
+++ thisisjava/src/day12/exercise/WrongPasswordException.java
@@ -1,6 +1,8 @@
 package day12.exercise;
 
 public class WrongPasswordException extends Exception {
+	
 	public WrongPasswordException() {}
 	public WrongPasswordException(String message) {super(message);}
+	
 }
