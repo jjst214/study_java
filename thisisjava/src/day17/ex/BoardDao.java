d6df011e9d202c9deacf37d9dd81210d4c2bae62
jjst214 <jjst214@gmail.com>
2023-05-15 17:51:36 +0900

    0515 Lamda

--- thisisjava/src/day17/ex/BoardDao.java
+++ thisisjava/src/day17/ex/BoardDao.java
@@ -9,13 +9,10 @@ public class BoardDao {
 	public List<Board> getBoardList() {
 		
 		List<Board> list = new ArrayList<Board>();
-		Board board = new Board("제목1", "내용1");
-		Board board2 = new Board("제목2", "내용2");
-		Board board3 = new Board("제목3", "내용3");
 		
-		list.add(board);
-		list.add(board2);
-		list.add(board3);
+		list.add(new Board("제목1", "내용1"));
+		list.add(new Board("제목2", "내용2"));
+		list.add(new Board("제목3", "내용3"));
 		
 		return list;
 		
--- thisisjava/src/day18/ex/MapExam690p.java
+++ thisisjava/src/day18/ex/MapExam690p.java
@@ -1,8 +1,6 @@
 package day18.ex;
 
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -20,7 +18,16 @@ public class MapExam690p {
 		int totalScore = 0;
 		
 		Set<Entry<String, Integer>> set = map.entrySet();
-		
+		for(Entry<String, Integer> entry: set) {
+			if(entry.getValue()>maxScore) {
+				maxScore = entry.getValue();
+				name = entry.getKey();
+			}
+			totalScore += entry.getValue();
+		}
+		System.out.println("평균 점수: " + (totalScore / set.size()));
+		System.out.println("최고 점수: " + maxScore);
+		System.out.println("최고 점수를 받은 아이디: " + name);
 		
 	}
 
